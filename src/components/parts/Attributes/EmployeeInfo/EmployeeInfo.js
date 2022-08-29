e1aff86be31e033d997f068bc70728a7100f924f
ltchelishvili1 <tchelishvililevani@gmail.com>
2022-08-29 14:04:45 +0400

    EmployeeInfo doesnt change on refresh

--- src/components/parts/Attributes/EmployeeInfo/EmployeeInfo.js
+++ src/components/parts/Attributes/EmployeeInfo/EmployeeInfo.js
@@ -1,27 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './EmployeeInfo.css'
 const EmployeeInfo = (props) => {
-  const [EmpInfo, setEmpInfo] = useState({
+  const [EmpInfo, setEmpInfo] = useState( JSON.parse(localStorage.getItem("EmpInfo")) || {
     name: "",
     surname: "",
-    team: "",
-    position: {
+    team: {
       name: "",
-      id : 0
+      team_id : 0
     },
     mail: "",
     number: ""
 
   })
 
+ 
+  useEffect(() => {
+    localStorage.setItem('EmpInfo', JSON.stringify(EmpInfo));
+  }, [EmpInfo]);
+
   return (
+    
     <div className='empinfo'>
       {console.log(EmpInfo)}
       <form>
         <div className='namesurname'>
           <div className='name'>
             <p>სახელი</p>
-            <input type="text" onChange={(e) =>{
+            <input value={EmpInfo.name} type="text" onChange={(e) =>{
                setEmpInfo({
                 ...EmpInfo,
                 name: e.target.value
@@ -31,7 +36,7 @@ const EmployeeInfo = (props) => {
           </div>
           <div className='surname'>
             <p>გვარი</p>
-            <input type="text"  onChange={(e) =>{
+            <input value={EmpInfo.surname} type="text"  onChange={(e) =>{
                setEmpInfo({
                 ...EmpInfo,
                 surname: e.target.value
@@ -42,41 +47,54 @@ const EmployeeInfo = (props) => {
         </div>
         <div className='selects'>
           <div className='team'>
-            <select  onChange={(e) =>{
+            <select value={`{"name":"${EmpInfo.team.name}","id":"${EmpInfo.team.team_id}"}`}  id='mySelect' onChange={(e) =>{
                   setEmpInfo({
                     ...EmpInfo,
-                    position:{
+                    team:{
                         name: JSON.parse(e.target.value).name,
-                        id: JSON.parse(e.target.value).id
+                        team_id: JSON.parse(e.target.value).id
                     }
                   })
                }}>
-               <option value="none" selected disabled hidden>თიმი</option>
               {props.teams.map((team) => (
-                <option value={`{"name":"${team.name}","id":"${team.id}"}`} key={team.id}>{team.name}</option>
+                <option  value={`{"name":"${team.name}","id":"${team.id}"}`}  key={team.id}>{team.name}</option>
               ))}
             </select>
           </div>
           <div className='position'>
-            <select>
+            <select value={EmpInfo.position}  onChange={(e) =>{
+               setEmpInfo({
+                ...EmpInfo,
+                position: e.target.value
+              })
+            }}>
             <option value="none" selected disabled hidden>პოზიცია</option>
                 {props.positions.filter((position)=>
-                (  position.team_id==EmpInfo.position.id)
+                (  position.team_id==EmpInfo.team.team_id)
                 ).map((filteredPosition)=>(
-                  <option>{filteredPosition.name}</option>
+                  <option value={filteredPosition.name} >{filteredPosition.name}</option>
                 ))}
-                
-
             </select>
+
           </div>
         </div>
         <div className='mail'>
           <p>მეილი</p>
-          <input type="mail" value="" />
+          <input value={EmpInfo.mail} type="mail"  onChange={(e) =>{
+               setEmpInfo({
+                ...EmpInfo,
+                mail: e.target.value
+              })
+            }}/>
         </div>
         <div className='number'>
           <p>ნომერი</p>
-          <input type="text" value="" />
+          <input value={EmpInfo.number} type="text" onChange={(e) =>{
+               setEmpInfo({
+                ...EmpInfo,
+                number: e.target.value
+              })
+            }} />
         </div>
 
       </form>
