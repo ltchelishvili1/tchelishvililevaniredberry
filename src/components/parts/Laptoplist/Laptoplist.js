e08d24dc26f6cee9c5bafef8f0e70c0adaeb4e07
ltchelishvili1 <tchelishvililevani@gmail.com>
2022-08-31 18:08:33 +0400

    updated list

--- src/components/parts/Laptoplist/Laptoplist.js
+++ src/components/parts/Laptoplist/Laptoplist.js
@@ -3,19 +3,21 @@ import './Laptoplist.css'
 const Laptoplist = (props) => {
     return (
         <div className='laptoplist'>
-
             <p className='notelistname'>ჩანაწერების სია</p>
             {console.log(props.laptops)}
             <div className='containernotes'>
                 <div className='notelist'>
                     {props.laptops.map((x) => (
-                        <div key={x.laptop.id}>
+                        <div className='laptopcont' key={x.laptop.id}>
                             {
                                 console.log(x.laptop.image)
                             }
-                            <img src={"https://pcfy.redberryinternship.ge/"+ x.laptop.image} alt="img" />
-                            <p>{x.user.name}{" "} {x.user.surname}</p>
-                            <p>{x.laptop.name}</p>
+                            <img src={"https://pcfy.redberryinternship.ge/" + x.laptop.image} alt="img" />
+                            <div className='rightcntent'>
+                                <p className='username'>{x.user.name}{" "} {x.user.surname}</p>
+                                <p className='laptopname'>{x.laptop.name}</p>
+                                <p className='more'>მეტის ნახვა</p>
+                            </div>
                         </div>
                     ))}
                 </div>
